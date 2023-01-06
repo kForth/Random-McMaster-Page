8f9b56f67559841ee1e2b7993dcc091c664d4bb6
Kestin Goforth <kgoforth1503@gmail.com>
2023-01-06 22:16:07 +0000

    Update to use sitemap

--- random_mcmaster_part.js
+++ random_mcmaster_part.js
@@ -1,20 +1,36 @@
 var observer = new MutationObserver(function (e) {
   // Check for existing link
-  if (document.getElementById("BottomNavWebPart_RandomPart")) return;
+  if (document.getElementById("BottomNavWebPart_RandomPage")) return;
 
   // Create new link
   var link = document.createElement("a");
-  link.textContent = "Random Part";
+  link.textContent = "Random Page";
   link.classList.add("BottomNavLnk");
-  link.setAttribute("id", "BottomNavWebPart_RandomPart");
+  link.setAttribute("id", "BottomNavWebPart_RandomPage");
   link.setAttribute("href", "javascript:void(0);");
-  link.addEventListener("click", function () {
-    // TODO: Somehow verify this is an actual part number.
-    var partNum =
-      (Math.random() * 1e5).toFixed(0) +
-      String.fromCharCode(Math.floor(Math.random() * 26) + 65) +
-      (Math.random() * 1e3).toFixed(0).padStart(3, "0");
-    window.location = "https://www.mcmaster.com/" + partNum;
+  link.addEventListener("click", async function () {
+    // Add loading icon
+    var loader = document.createElement("div");
+    loader.classList.add("ShellLayout_WaitIcon");
+    var loaderParent = document.getElementById("MainContent");
+    var contentDiv = loaderParent.childNodes[0]
+    loaderParent.insertBefore(loader, contentDiv);
+
+    try {
+      // Load sitemap and extract links
+      var response = await fetch("https://www.mcmaster.com/sitemap.xml", {
+        mode: 'same-origin',
+        referrerPolicy: 'origin',
+      });
+      var content = await response.text();
+      var links = [...content.matchAll(/<loc>(.*)<\/loc>/g)].map(m => m[1]);
+      // Choose random link
+      var link = links[Math.floor(Math.random() * links.length)];
+      window.location = link;
+    } finally {
+      // Remove loading icon
+      loader.remove();
+    }
   });
 
   // Create seperator
